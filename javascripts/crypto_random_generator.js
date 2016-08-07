7636911a4358d6f1a5e1470ac777086fe259ec85
Daniel Stiner <danstiner@gmail.com>
2016-08-06 23:15:28 -0700

    Remove license headers

--- javascripts/crypto_random_generator.js
+++ javascripts/crypto_random_generator.js
@@ -1,24 +1,3 @@
-/*
-    Copyright (c) 2016 Daniel Stiner
-
-    Permission is hereby granted, free of charge, to any person obtaining
-    a copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the Software
-    is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
 (function(exports) {
   var max_uint32 = 4294967295;
 
@@ -50,7 +29,7 @@
 
     var randomValues = new Uint32Array(1);
     window.crypto.getRandomValues(randomValues);
-    
+
     return {
       symbol: alphabet[randomValues[0] % alphabet.length],
       bitsOfEntropy: Math.log2(alphabet.length)
