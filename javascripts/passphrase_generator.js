19be894b306c9b12e58236b7ed3609a0a8e55355
Daniel Stiner <danstiner@gmail.com>
2016-08-06 22:55:47 -0700

    Split out robust getRandomSymbol function

--- javascripts/passphrase_generator.js
+++ javascripts/passphrase_generator.js
@@ -20,6 +20,25 @@
     OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 (function(exports) {
+  var max_uint32 = 4294967295;
+
+  if (!Array.prototype.fill) {
+    Array.prototype.fill = function(value) {
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      var O = Object(this);
+      var len = O.length >>> 0;
+
+      for (var k = 0; k < len; k++) {
+        O[k] = value;
+      }
+
+      return O;
+    };
+  };
+
   exports.generatePassphrase = function(alphabet, count) {
     try {
       var result = getRandomSymbols(alphabet, count);
@@ -29,14 +48,30 @@
       displayError(ex);
       throw ex;
     } 
-  }
+  };
 
   function getRandomSymbols(alphabet, count) {
-    var randomValues = new Uint32Array(count);
+    return Array(count).fill(alphabet).map(getRandomSymbol).reduce(
+      (previousValue, currentValue) => ({
+        symbols: previousValue.symbols.concat(currentValue.symbol),
+        bitsOfEntropy: (previousValue.bitsOfEntropy + currentValue.bitsOfEntropy)
+      }),
+      {symbols: [], bitsOfEntropy: 0});
+  }
+
+  function getRandomSymbol(alphabet) {
+    if (alphabet.length >= max_uint32) {
+      throw new RangeError("Symbol alphabet too large");
+    }
+    if(!window.crypto || !window.crypto.getRandomValues)
+    {
+      throw new Error("Browser does support secure randomness generation")
+    }
+    var randomValues = new Uint32Array(1);
     window.crypto.getRandomValues(randomValues);
     return {
-      symbols: Array.from(randomValues).map(value => alphabet[value % alphabet.length]),
-      bitsOfEntropy: count * Math.log2(alphabet.length)
+      symbol: alphabet[randomValues[0] % alphabet.length],
+      bitsOfEntropy: Math.log2(alphabet.length)
     };
   }
 
