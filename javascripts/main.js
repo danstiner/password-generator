b524b9641acb002c7ddc3da7532b17cd9f3c1d04
Daniel Stiner <danstiner@gmail.com>
2018-01-21 18:44:24 -0800

    Set a content security policy

---
+++ javascripts/main.js
@@ -0,0 +1,7 @@
+var onSelectWordCount = passphrase_generator.setWordCount;
+var onSelectWordlist = passphrase_generator.setWordlist;
+var selectNodeContents = passphrase_generator.selectNodeContents;
+
+window.onload = () => {
+  passphrase_generator.run();
+};
