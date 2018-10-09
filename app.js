dbd019fd0e920169fb5659e1086fe3d6a7c6ece2
Daniel Stiner <daniel.stiner@gmail.com>
2018-10-08 21:44:01 -0700

    Combine app javascript to single file

--- app.js
+++ app.js
@@ -1,4 +1,86 @@
 'use strict';
+
+(function(exports) {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+  var MAX_SAFE_INTEGER = 9007199254740991;
+
+  if (!Array.prototype.fill) {
+    Array.prototype.fill = function(value) {
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      var O = Object(this);
+      var len = O.length >>> 0;
+
+      for (var k = 0; k < len; k++) {
+        O[k] = value;
+      }
+
+      return O;
+    };
+  };
+
+  function getRandomSymbol(alphabet) {
+    if (!window.crypto || !window.crypto.getRandomValues)
+    {
+      throw new Error("Browser does support secure randomness generation")
+    }
+
+    var index = secureRandom(0, alphabet.length);
+
+    return {
+      alphabet: alphabet,
+      symbol: alphabet[index],
+      bitsOfEntropy: Math.log2(alphabet.length)
+    };
+  };
+
+  // Generate a random number in [min, max) using secure window.crypto.getRandomValues API
+  // For non-power-of-two ranges, rejection sampling is used to achieve an unbiased distribution
+  function secureRandom(min, max) {
+    var range = max - min;
+
+    if (max > MAX_SAFE_INTEGER || range > MAX_SAFE_INTEGER) {
+      throw new RangeError("Maximum or range of random value larger than safely representable in JavaScript");
+    }
+  
+    var num_bits = Math.ceil(Math.log2(range));
+    var bitmask = Math.pow(2, num_bits) - 1;
+    var randomValue;
+
+    do {
+      var randomArray = new Uint32Array(2);
+      window.crypto.getRandomValues(randomArray);
+      // 21 upper bits + 32 lower bits = 53 bits (max safe integer)
+      var upper_uint32 = randomArray[0] >> 11;
+      var lower_uint32 = randomArray[1];
+      randomValue = (upper_uint32 << 32 | lower_uint32) & bitmask;
+    } while (randomValue > range)
+
+    return min + randomValue;
+  }
+
+  exports.getRandomSymbolsFromFixedAlphabet = function(alphabet, count) {
+      return concatRandomSymbols(Array(count).fill(alphabet).map(getRandomSymbol));
+  };
+
+  exports.getRandomSymbolsFromAlphabets = function(alphabets) {
+      return concatRandomSymbols(alphabets.map(getRandomSymbol));
+  };
+
+  function concatRandomSymbols(randomSymbols) {
+    return randomSymbols.reduce(
+      (previousValue, currentValue) => ({
+        alphabets: previousValue.alphabets.concat([currentValue.alphabet]),
+        symbols: previousValue.symbols.concat([currentValue.symbol]),
+        bitsOfEntropy: (previousValue.bitsOfEntropy + currentValue.bitsOfEntropy)
+      }),
+      {alphabets: [], symbols: [], bitsOfEntropy: 0});
+  };
+
+})(this.crypto_random_generator = {});
+
 (function(exports) {
   var generationAlgorithms = {
     "phrase": {
@@ -192,3 +274,14 @@
   };
 
 })(this.passphrase_generator = {});
+
+window.onload = () => {
+  var wordCountElems = document.querySelectorAll("#passphrase_generation_symbol_counts a");
+  for (var i = 0; i < wordCountElems.length; i++) {
+    wordCountElems[i].addEventListener("click", passphrase_generator.onChangeWordCount);
+  }
+
+  document.getElementById('generated_passphrase').addEventListener("click", passphrase_generator.selectNodeContents);
+
+  passphrase_generator.run();
+};
