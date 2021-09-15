96ee4174f630b3585ae9e92b3105070b0cf65a99
Daniel Stiner <danstiner@gmail.com>
2021-09-14 22:53:58 -0700

    WIP

--- src/generate-sentence.test.ts
+++ src/generate-sentence.test.ts
@@ -1,4 +1,4 @@
-import { randomUint32, randomInt } from './random'
+import { randomInt } from './random'
 import crypto from 'crypto'
 
 Object.defineProperty(window, 'crypto', {
@@ -68,16 +68,40 @@ function randomSentence() {
     const place_noun = [
         'abyss',
         'bakery',
+        'base',
         'moon',
         'windmill',
         'yard',
     ]
-    const any_noun = thing_noun.concat(place_noun)
-    const verbs = [
-        'ate',
-        'jumped',
-        'painted'
+    const location_noun = [
+        'the abyss',
+        'space',
+        'the waste',
     ]
+    const noun = thing_noun.concat(place_noun)
+    const intransitive_complete_verb = [
+        'bark',
+        'cry',
+        'die',
+        'laugh',
+        'fart',
+        'sneeze'
+    ]
+    const sense_verb = [
+        'hear',
+        'see',
+        'feel',
+        'taste',
+        'smell'
+    ]
+    const verb = [
+        'abandon',
+        'eat',
+        'hear',
+        'jump',
+        'paint',
+        'shop',
+    ].concat(sense_verb).concat(intransitive_complete_verb)
     const modifier = [
         'abandoned',
         'abbreviated',
@@ -113,6 +137,7 @@ function randomSentence() {
         'fine',
         'flashy',
         'freezing',
+        'French',
         'godless',
         'gold',
         'grand',
@@ -131,7 +156,7 @@ function randomSentence() {
         'sexy',
         'unknown',
     ]
-    const prepositions = [
+    const preposition = [
         'along',
         'below',
         'concerning',
@@ -142,16 +167,95 @@ function randomSentence() {
         'through',
         'with',
     ]
+    const indefinite_pronoun = [
+        'another',
+        'any',
+        'anybody',
+        'anyone',
+        'anything',
+        'both',
+        'each',
+        'either',
+        'everybody',
+        'everyone',
+        'everything',
+        'many',
+        'neither',
+        'nobody',
+        'none',
+        'no one',
+        'one',
+        'other',
+        'others',
+        'some',
+        'somebody',
+        'someone'
+    ]
+    const qualifier = [
+        'always',
+        'never',
+        'sometimes',
+    ]
+    const subject = [
+        'I',
+        'we',
+    ].concat(noun.map(noun => 'the ' + noun))
+    const subject2 = [
+        'I',
+        'we',
+        'the French',
+        'Eskimos',
+        'Americans',
+        'Brits',
+    ]
+    const get = [
+        'find',
+        'get',
+        'know',
+        'see',
+    ]
+    const love = [
+        'eat',
+        'hate',
+        'love',
+    ]
+
+    const choosen_noun1 = choose(noun)
+
+    // phrasal templates
+    // snowclones
+    const sentence = [
+        // digit the
+        `${randomInt(100)} ${choose(modifier)} ${choose(thing_noun)}s ${choose(verb)} ${choose(preposition)} the ${choose(modifier)} ${choose(noun)}`,
+        // c
+        // `Crying is for ${choose(modifier)} ${choosen_noun1}s. ${choose(modifier)} ${choosen_noun1}s go ${choose(verb)}`,
+        // d - Draw me like one of your French girls
+        // `Draw ${choose(object)} like one of your ${choose(modifier)} ${choose(any_noun)}s`,
+        // i - If Eskimos have N words for snow, X surely have Y words for Z
+        `If Eskimos have ${randomInt(100)} words for ${choose(noun)}s, ${choose(subject2)} surely have ${randomInt(10)} words for ${choose(noun)}s`,
+        // i - In X, no one can hear you Y
+        `In ${choose(location_noun)}, ${choose(indefinite_pronoun)} can ${choose(sense_verb)} you ${choose(intransitive_complete_verb)}`,
+        // i - I'm in your X, Y-ing your Zz
+        `I'm in your ${choose(place_noun)}, ${choose(verb)}-ing your ${choose(modifier)} ${choose(thing_noun)}`,
+        // "X, Y, and Z, oh my!".
+        // m - Mama always said 'Life was like a box of chocolates, you never know what you’re gonna get.' — Forrest Gump
+        //`Mama always said 'a ${choose(thing_noun)} is like a box of ${choose(thing_noun)}s, you ${choose(qualifier)} know what you're gonna ${choose(get)}'`,
+        // M - May the force with with you.
+        //`May the ${choose(modifier)} ${choose(any_noun)} be ${choose(preposition)} you ${choose(qualifier)}`,
+        // "To X or not to X? That is the question"
+        // w
+        `Welcome to the ${choose(modifier)} ${choose(thing_noun)}. It's ${choose(modifier)}. You're gonna ${choose(love)} it!`,
+    ]
 
     return {
-        sentence: `${randomInt(100)} ${choose(modifier)} ${choose(thing_noun)}s ${choose(verbs)} ${choose(prepositions)} the ${choose(modifier)} ${choose(any_noun)}`,
-        entropy: Math.log2(100 * modifier.length * thing_noun.length * verbs.length * prepositions.length * modifier.length + any_noun.length)
+        sentence: choose(sentence),
+        entropy: Math.log2(100 * modifier.length * thing_noun.length * verb.length * preposition.length * modifier.length + noun.length)
     }
 }
 
 describe('randomSentence', () => {
     it('generates a memorable passphrase with sufficient entropy', () => {
-        for(var i=0; i<5; i++) {
+        for (var i = 0; i < 10; i++) {
             console.log(randomSentence().sentence)
         }
         expect(randomSentence().entropy).toBeGreaterThan(28)
